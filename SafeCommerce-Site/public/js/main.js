81a531434d1bb67f5b571d16a5809f85553fc8f7
ryan-miyazato <ryan.miyazato@sptech.school>
2022-08-15 12:33:53 -0300

    Header automatizado

--- SafeCommerce-Site/public/js/main.js
+++ SafeCommerce-Site/public/js/main.js
@@ -1,7 +1,25 @@
 class myHeader extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
+        <div class="header">
+        <div class="container">
+            <div class="marca">
+                <img class="logo" src="assets/logo-img.png" alt="">
+                <span class="logo-titulo">SafeCommerce</span>
+            </div>
+            <div class="navBar">
+                <a class="item-navBar aqui">Home</a>
+                <a class="item-navBar">Serviços</a>
+                <a class="item-navBar">Sobre Nós</a>
+                <a class="item-navBar">Contato</a>
+            </div>
+            <div class="registro">
+                <button class="but-login">Login</button>
+                <button class="but-cadastro">Cadastro</button>
+            </div>
 
+        </div>
+    </div>
         `
     }
 }
