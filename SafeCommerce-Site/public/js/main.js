808137e2497955c99ddf0590b26aab6f291e8d68
Coimbra1999 <guilherme.brito@sptech.school>
2022-08-19 22:53:24 -0300

    Cadastro e login (incompletos)

--- SafeCommerce-Site/public/js/main.js
+++ SafeCommerce-Site/public/js/main.js
@@ -14,8 +14,8 @@ class myHeader extends HTMLElement {
                 <a id="nav_download" href="download.html" class="item-navBar">Download</a>
             </div>
             <div class="registro">
-                <button class="but-login">Login</button>
-                <button class="but-cadastro">Cadastro</button>
+                <a href="Login.html"><button class="but-login">Login</button></a>
+                <a href="Cadastro.html"><button class="but-cadastro">Cadastro</button></a>
             </div>
         </div>
     </div> 
@@ -24,3 +24,28 @@ class myHeader extends HTMLElement {
 }
 
 customElements.define('my-header', myHeader);
+
+class myFooter extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <div class="footer">
+        <div class="container">
+            <div class="navBar">
+                <a href="" class="item-navBar">Termos de Uso</a>
+                <a href="" class="item-navBar">Privacidade</a>
+                <a href="" class="item-navBar">Cookies</a>
+                <a href="" class="item-navBar">Política da Empresa</a>
+                <a href="" class="item-navBar">Contato</a>
+            </div>
+            <div class="marca">
+                <img class="logo" src="assets/logo-img.png" alt="">
+                <span class="logo-titulo">SafeCommerce</span>
+                <span class="logo-trademark">© 2022 SafeCommerce Inc. Todos os direitos reservados.</span>
+            </div>
+        </div>
+    </div> 
+        `
+    }
+}
+
+customElements.define('my-footer', myFooter);
---
+++ SafeCommerce-Site/src/models/usuarioModel.js
@@ -0,0 +1,42 @@
+var database = require("../database/config")
+
+function listar() {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
+    var instrucao = `
+        SELECT * FROM usuario;
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+function entrar(email, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
+    var instrucao = `
+        SELECT * FROM Usuario WHERE email = '${email}' AND senha = '${senha}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+// Coloque os mesmos parâmetros aqui. Vá para a var instrucao
+console.log("passando valores como parametro")
+
+function cadastrar(nome, cnpj, email, senha) {
+    console.log(nome, cnpj, email, senha);
+    
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+    var instrucao = `
+        INSERT INTO Usuario (nome, cnpj, email, senha) VALUES ('${nome}', '${cnpj}', '${email}', '${senha}');
+    `;
+            console.log("Executando a instrução SQL: \n" + instrucao);
+            return database.executar(instrucao);
+        }
+
+console.log("Instrução mysql")
+
+module.exports = {
+    entrar,
+    cadastrar,
+    listar
+};
