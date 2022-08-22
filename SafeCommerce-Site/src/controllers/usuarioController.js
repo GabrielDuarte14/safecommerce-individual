393c32325f9ce1a89b9bb7d916755e24d484421b
KauanCavazani <kauan.cavazani@gmail.com>
2022-08-22 10:51:53 -0300

    delete: arquivos desnecessarios

--- SafeCommerce-Site/src/controllers/usuarioController.js
+++ SafeCommerce-Site/src/controllers/usuarioController.js
@@ -1,29 +1,5 @@
 var usuarioModel = require("../models/usuarioModel");
 
-var sessoes = [];
-
-function testar(req, res) {
-    console.log("ENTRAMOS NA usuarioController");
-    res.json("ESTAMOS FUNCIONANDO!");
-}
-
-function listar(req, res) {
-    usuarioModel.listar()
-        .then(function (resultado) {
-            if (resultado.length > 0) {
-                res.status(200).json(resultado);
-            } else {
-                res.status(204).send("Nenhum resultado encontrado!")
-            }
-        }).catch(
-            function (erro) {
-                console.log(erro);
-                console.log("Houve um erro ao realizar a consulta! Erro: ", erro.sqlMessage);
-                res.status(500).json(erro.sqlMessage);
-            }
-        );
-}
-
 function entrar(req, res) {
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
@@ -105,7 +81,5 @@ function cadastrar(req, res) {
 
 module.exports = {
     entrar,
-    cadastrar,
-    listar,
-    testar
+    cadastrar
 }
